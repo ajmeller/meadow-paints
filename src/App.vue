d98b1d87615f06046c9bba44dba5098388e9d70a
Alyssa Meller <ameller@nhaschools.com>
2025-06-18 16:01:59 -0400

    Add vercel.json + speed insights

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
 
 <template>
   <div class="side-by-side">
     <NavBar />
     <RouterView />
+    <SpeedInsights />
   </div>
 </template>
